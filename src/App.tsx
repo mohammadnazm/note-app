21223305be113e157d400f0a519ecc3bfbbf2397
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-18 13:02:36 +0300

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes, Route, Navigate } from "react-router-dom"
+
 function App() {
-  return <h1>Hi</h1>
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/new" element={<h1>New</h1>} />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  )
 }
 
 export default App
