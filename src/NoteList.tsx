c31c0c34a396f2aa07dcb3f403c696e870743742
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-19 17:03:03 +0300

    upgrade

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,16 +1,36 @@
-import { useState } from "react"
-import { Button, Col, Form, Row, Stack } from "react-bootstrap"
+import { useMemo, useState } from "react"
+import { Button, Card, Col, Form, Row, Stack } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import ReactSelect from "react-select"
 import { Tag } from "./App"
+import styles from "./NotesList.module.css"
+
+type SimplifiedNote = {
+  tags: Tag[]
+  title: string
+  id: string
+}
 
 type NoteListProps = {
   availableTags: Tag[]
 }
 
-export function NoteList({ availableTags }: NoteListProps) {
+export function NoteList({ availableTags, notes }: NoteListProps) {
   const [selectedTags, setSelectedTags] = useState<Tag[]>([])
   const [title, setTitle] = useState("")
+
+  const filteredNotes = useMemo(() => {
+    return notes.filter(note => {
+      return (
+        (title === "" ||
+          note.title.toLowerCase().includes(title.toLowerCase())) &&
+        (selectedTags.length === 0 ||
+          selectedTags.every(tag =>
+            note.tags.some(noteTag => noteTag.id === tag.id)
+          ))
+      )
+    })
+  }, [title, selectedTags, notes])
 
   return (
     <>
@@ -62,6 +82,32 @@ export function NoteList({ availableTags }: NoteListProps) {
           </Col>
         </Row>
       </Form>
+      <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
+        {filteredNotes.map(note => (
+          <Col key={note.id}>
+            <NoteCard id={note.id} title={note.title} tags={note.tags} />
+          </Col>
+        ))}
+      </Row>
     </>
+  )
+}
+
+function NoteCard({ id, title, tags }: SimplifiedNote) {
+  return (
+    <Card
+      as={Link}
+      to={`/${id}`}
+      className={`h-100 text-reset text-decoration-none ${styles.card}`}
+    >
+      <Card.Body>
+        <Stack
+          gap={2}
+          className="align-items-center justify-content-center h-100"
+        >
+          <span className="fs-5">{title}</span>
+        </Stack>
+      </Card.Body>
+    </Card>
   )
 }
